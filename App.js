9b9d35255c9aa6c2f36faa171cf04d058731b25e
Rob Peschke <rspesc31@gmail.com>
2020-07-27 16:38:27 -0500

    added example of BarChart

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+
+// import Histogram from 'react-native-histogram';
+
+const screenWidth = Dimensions.get("window").width;
+
+// const data = {
+//   labels: ["January", "February", "March", "April", "May", "June"],
+//   datasets: [
+//     {
+//       data: [20, 45, 28, 80, 99, 43]
+//     }
+//   ]
+// };
+// const data = [{ data: [10, 13, 8, 19, 17] }]
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app rob!</Text>
       <StatusBar style="auto" />
+      {/* <Histogram data={data} /> */}
+      <BarChart
+        data={{
+          labels: [
+            'Jan',
+            'Feb',
+            'March',
+            'April',
+            'May',
+            'June',
+          ],
+          datasets: [
+            {
+              data: [10, 0, 12, 13, 3, 2, 1],
+            },
+          ],
+        }}
+        width={Dimensions.get('window').width - 20}
+        height={220}
+        yAxisLabel={''}
+        chartConfig={{
+          backgroundColor: 'red',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 0,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          // color: '#F67451',
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 10,
+          borderRadius: 16,
+        }}
+      />
     </View>
   );
 }

86e83fff563ada8798b548b56b3adb153c21b481
Rob Peschke <rspesc31@gmail.com>
2020-08-12 11:48:52 -0400

    successfully used the expo image picker

--- App.js
+++ App.js
@@ -1,32 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Dimensions, } from 'react-native';
-import HistogramBar from './HistogramBar';
-// import { RNTextSwitch } from 'react-native-text-switch';
-import SlideComponent from './SlideComponent'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions, Button, Image } from 'react-native';
 import { Picker } from '@react-native-community/picker';
-import ImagePicker from 'react-native-image-picker';
-
-
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import HistogramBar from './HistogramBar';
+import SlideComponent from './SlideComponent'
+import { getPermissionsAsync } from 'expo-location';
 
 const data = {
   test: [{ votes: 1, grade: 'V4' }, { votes: 10, grade: 'V4+' }, { votes: 11, grade: 'V5-' }, { votes: 20, grade: 'V5' }, { votes: 9, grade: 'V5+' }, { votes: 3, grade: 'V6-' }, { votes: 1, grade: 'V6' }]
 }
 
-export default function App() {
+function App() {
   let max = Math.max.apply(Math, data.test.map(function (i) { return i.votes; }))
-  const [language, changeLanguage] = useState('java');
+  // const [language, changeLanguage] = useState('java');
   const [image, setImage] = useState(null);
 
-  const handleChoosePhoto = () => {
-    const options = {
-      noData: true,
-    };
-    ImagePicker.launchImageLibrary(options, response => {
-      if (response.uri) {
-        this.setState({ photo: response });
+  useEffect(() => {
+    getPermissionsAsync();
+  }, [])
+
+  const getPermissionsAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
       }
-    });
+    }
+  }
+
+  const handleChoosePhoto = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        setImage(result)
+        console.log('result', result)
+        console.log(result.uri)
+
+        // this.setState({ image: result.uri });
+      }
+
+      // console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
   };
 
   return (
@@ -36,7 +60,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app rob!</Text>
       <StatusBar style="auto" />
       {/* <SlideComponent/> */}
-      <Picker
+      {/* <Picker
         selectedValue={language}
         style={{ height: 50, width: 200, backgroundColor: 'turquoise' }}
         onValueChange={(itemValue, itemIndex) =>
@@ -47,7 +71,7 @@ export default function App() {
         <Picker.Item label="Kotlin" value="kt" />
         <Picker.Item label="JSON" value="json" />
         <Picker.Item label="C Sharp" value="C#" />
-      </Picker>
+      </Picker> */}
 
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         {image && (
@@ -56,7 +80,10 @@ export default function App() {
             style={{ width: 300, height: 300 }}
           />
         )}
-        <Button title="Choose Photo" onPress={handleChoosePhoto} />
+        <Button
+          title="Choose Photo"
+          onPress={handleChoosePhoto}
+        />
       </View>
       {/* <View style={{
         width: '100%',
@@ -87,3 +114,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
